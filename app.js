eb543df61145d6853ab082b05e59235245a38c52
heunyam <tpwns072@gmail.com>
2021-02-17 19:35:55 +0900

    feat: Add sendImage API

--- app.js
+++ app.js
@@ -49,10 +49,27 @@ io.on('connection', socket => {
         };
 
         messageData = await postMessageData(jsonData);
-        socket.to(data['roomId'].toString()).emit('realTimeChatting', messageData);
-        console.log(messageData)
+        socket.broadcast.to(data['roomId'].toString()).emit('realTimeChatting', messageData);
 
-        console.log(`[LOG] 실시간 채팅 이벤트`);
+        console.log(`[LOG] ${data['roomId']} 에서 실시간 채팅 이벤트`);
+        console.log(messageData)
+    });
+
+    socket.on('sendImage', async (data) => {
+        console.log(`[LOG] ${data['roomId']} 방에서 이미지 전송됨`);
+
+        jsonData = {
+            "roomId": data['roomId'],
+            "token": data['token'],
+            "content": data['message'],
+            "type": MessageType.photo
+        };
+
+        messageData = await postMessageData(jsonData);
+        socket.broadcast.to(data['roomId'].toString()).emit('realTimeChatting', messageData);
+
+        console.log(`[LOG] ${data['roomId']} 에서 실시간 채팅 이벤트`);
+        console.log(messageData)
     });
 });
 
